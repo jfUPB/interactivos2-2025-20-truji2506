cab02f1e41abe098b4f16339ae1e6212599d8df8
truji2506 <jose.trujilloc@upb.edu.co>
2025-10-16 15:08:29 -0500

    Proyecto

---
+++ unidad-7/proyecto/MobileClient/script.js
@@ -0,0 +1,54 @@
+const socket = io("/MobileClient");
+let myId = null;
+let myColor = { r: 255, g: 255, b: 255 };
+const meId = document.getElementById("meId");
+const meHex = document.getElementById("meHex");
+
+const picker = document.getElementById("picker");
+const debug = document.getElementById("debug");
+
+function hexToRgbObject(hex) {
+  const v = hex.replace("#", "");
+  const bigint = parseInt(v, 16);
+  return {
+    r: (bigint >> 16) & 255,
+    g: (bigint >> 8) & 255,
+    b: bigint & 255
+  };
+}
+
+function rgbToHex({ r, g, b }) {
+  return (
+    "#" +
+    [r, g, b]
+      .map(n => Math.max(0, Math.min(255, n | 0)).toString(16).padStart(2, "0"))
+      .join("")
+  );
+}
+
+let current = { user_color: { r: 255, g: 255, b: 255 } };
+
+function renderDebug(payload) {
+  debug.textContent = JSON.stringify(payload, null, 2);
+}
+
+socket.on("state:init", (s) => {
+  current = s.cel;
+  picker.value = rgbToHex(s.cel.user_color);
+  renderDebug(s);
+});
+
+socket.on("state", (payload) => renderDebug(payload.state));
+socket.on("you", ({ id, color }) => {
+  myId = id;
+  myColor = color;
+  meId.textContent = id;
+  meHex.textContent = rgbToHex(color);
+  // sincroniza el picker con tu color real en el server
+  picker.value = rgbToHex(color);
+});
+
+picker.addEventListener("input", () => {
+  current.user_color = hexToRgbObject(picker.value);
+  socket.emit("update", current);
+});
